daf05daa56cddaba14010af75c15e817023a39a3
Ricky Tan <rickytan@abstract-assembly.com>
2021-04-23 14:07:57 -0400

    got board to display

---
+++ src/ChessRival.py
@@ -0,0 +1,26 @@
+"""
+Author: Richard Matthew Samonte Tan (rickytan-AA@github.com)
+Version: 2021.04.23
+License: MIT
+"""
+
+# ---------------------------------------------------------- Imports
+# chess
+import chess
+
+# common & scientific libraries
+import random
+
+# ---------------------------------------------------------- Class Definition
+class ChessRival:
+    """
+    Creates an instance of a ChessRival bot.
+    """
+    def __init__(self):
+        pass
+
+# ---------------------------------------------------------- Main
+if __name__ == '__main__':
+    board = chess.Board()
+    print(board)
+    print(board.legal_moves)
