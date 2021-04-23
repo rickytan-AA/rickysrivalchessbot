dd853b138b90ea9e6732541fdcce1738b42a0741
Ricky Tan <rickytan@abstract-assembly.com>
2021-04-23 15:04:35 -0400

    got sequence for scholar's mate to work

--- src/ChessEnv.py
+++ src/ChessEnv.py
@@ -14,24 +14,67 @@ import random
 # ---------------------------------------------------------- Class Definition
 class ChessEnv:
     """
-    Creates an instance of a chessboard environment
+    Creates an instance of a chessboard environment.
     """
     def __init__(self):
-        self.board = self.reset()
+        self.board = None # initialized by reset()
 
-    # ------------------------------------------------------ Methods
+    # ------------------------------------------------------ RL Methods
     def reset(self):
         """
         Returns a new chessboard with the default piece layout.
         """
-        return chess.Board()
-        
+        self.board = chess.Board()
+
+        # return board state and done status
+        return self.board, self.board.is_checkmate()
+
+
+    def step(self,a):
+        """
+        Moves a piece on the board according to action a.
+        """
+        a = self.board.parse_san(a) # SAN move representation
+        if a not in self.board.legal_moves: # check if illegal
+            print("%s is an illegal move!"%a)
+            return
+        env.board.push(a) # perform if legal
+
+        # return board state and done status
+        return self.board, self.board.is_checkmate()
+
+
+    # ------------------------------------------------------ RL Methods
+    def step_seq(self,seq):
+        """
+        Plays a predetermined sequence of actions from a list.
+        """
+        # start board
+        current_board, done = self.reset()
+        print("---------------- \n")
+        print(current_board, "\n Done? %s"%done)
+
+        # begin sequence
+        for i,a in enumerate(seq):
+            current_board, done = self.step(a)
+            print("---------------- \n")
+            print(current_board, "\n Done? %s"%done)
+
+            if done:
+                print("Sequence ended in checkmate at move %s in index %s"%(a,i))
+                return current_board, done
+
+        print("Sequence finished.")
+        return current_board, done
+
 
 # ---------------------------------------------------------- Main
 if __name__ == '__main__':
     env = ChessEnv()
-    print(env.board)
 
-    env.board.push_san("e4")
-    print("\n")
-    print(env.board)
+    # scholar's mate
+    seq = ["e4", "e5", "Qh5", "Nc6", "Bc4", "Nf6", "Qxf7"]
+    current_board,done = env.step_seq(seq)
+    
+
+
